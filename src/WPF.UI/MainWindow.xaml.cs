5532461c566f508854ee71e1dd4ee6b8c4788447
Morten Brudvik <morten@brudvik.com>
2022-02-14 21:56:08 +0100

    feat: Drag and Drop file path into Blazor app
    Blazor-WebView2 that host a Blazor app, with
    support for dragging a file into the Blazor app using javascript interop.

---
+++ src/WPF.UI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace WPF.UI
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            webView2.EnsureCoreWebView2Async(null);
+            webView2.Source = new Uri("https://localhost:7225/");
+
+            webView2.CoreWebView2InitializationCompleted += (sender, args) =>
+            {
+                if (args.IsSuccess)
+                {
+                    webView2.CoreWebView2.NewWindowRequested += async (sender2, args2) =>
+                    {
+                        args2.Handled = true;
+
+                        var filePath = args2.Uri.ToString();
+                        
+                        await webView2.ExecuteScriptAsync($"interopFunctions.setFilePath('{filePath}');");
+                    };
+                    
+                }
+            };
+        }
+
+        private async void ButtonSetFilePath_Click(object sender, RoutedEventArgs e)
+        {
+            await webView2.ExecuteScriptAsync($"interopFunctions.setFilePath('{txtFilePath.Text}');");
+        }
+
+    }
+}
---
+++ src/Web/Client/wwwroot/script/winUIBlazorInterop.js
@@ -0,0 +1,18 @@
+﻿var interopFunctions = {};
+
+interopFunctions.registerIndexComponent = (dotnetObj) => {
+    interopFunctions.indexComponent = dotnetObj;
+};
+
+interopFunctions.setFilePath = (filePath) => {
+    console.log("JavaScript setFilePath executed");
+
+    interopFunctions.indexComponent.invokeMethodAsync('SetFilePath', filePath);
+};
+
+interopFunctions.setFilePathInWinWPF = (filePath) => {
+    window.chrome.webview.postMessage(JSON.stringify({
+        messageType: 'filePath',
+        value: filePath
+    }));
+};
